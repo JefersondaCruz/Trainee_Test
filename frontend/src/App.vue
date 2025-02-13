527a99dddd27512fb3e9784579e6f53f0c6376aa
JefersondaCruz <jefersondacruz40@gmail.com>
2025-02-13 15:23:13 -0300

    Mudando toda a estilização do Front End - Nova Temática

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,94 +1,25 @@
 <script setup>
 import { RouterLink, RouterView } from 'vue-router'
+import Home from './components/Home.vue'
 </script>
 
 <template>
   <div id="app">
-    <header>
-      <h1>Gerenciamento de Desenvolvedores</h1>
-      <nav>
-        <router-link to="/">Home</router-link>
-        <router-link to="/desenvolvedor">Desenvolvedores</router-link>
-        <router-link to="/niveis">Níveis</router-link>
-      </nav>
-    </header>
-    <main>
-      <RouterView />
-    </main>
+    <router-view></router-view>
   </div>
-  
 </template>
 
 <style scoped>
+@font-face {
+  font-family: 'Minecraftia';
+  src: url('@/assets/fonts/Minecraftia-Regular.ttf') format('truetype');
+}
+
 * {
   margin: 0;
   padding: 0;
   box-sizing: border-box;
-  
-}
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  text-align: center;
-  color: #2c3e50;
-  min-height: 100vh;
-  background-size: cover;
-  background-position: center;
-  position: relative;
-}
-
-#app::before {
-  content: "";
-  position: absolute;
-  top: 0;
-  left: 0;
-  right: 0;
-  bottom: 0;
-  background-image: url(../src/assets/capaDev.jpeg);
-  background-size: cover;
-  background-position: center;
-  filter: blur(10px);
-  z-index: -1;
-}
-
-
-header {
-  background-color: #35495e;
-  padding: 30px;
-  color: white;
-  display: flex;
-  justify-content: space-between;
-  align-items: center;
-  box-shadow: 0px 4px 6px rgba(0, 0, 0, 0.1);
-}
-
-h1 {
-  font-size: 1.8em;
-  font-weight: bold;
-}
-
-nav {
-  display: flex;
-  gap: 15px;
-}
-
-nav a {
-  color: white;
-  font-size: 1.4em;
-  text-decoration: none;
-  padding: 10px 15px;
-  border-radius: 5px;
-  transition: background-color 0.3s, transform 0.2s;
-  font-weight: bold;
-  transition: background-color 0.3s;
-}
-
-nav a:hover {
-  background-color: #42b983;
-  transform: scale(1.1);
-}
-
-main {
-  margin-top: 40px;
+  font-family: 'Minecraftia', sans-serif;
 }
 
 </style>
---
+++ frontend/src/components/Home.vue
@@ -0,0 +1,115 @@
+<template>
+  <div id="home">
+    <nav>
+      <router-link to="/desenvolvedor" class="mc-button">
+        <div class="title">Desenvolvedores</div>
+      </router-link>
+      <router-link to="/niveis" class="mc-button">
+        <div class="title">Níveis</div>
+      </router-link>
+    </nav>
+    <main>
+      <RouterView />
+    </main>
+  </div>
+</template>
+
+<script setup>
+</script>
+
+<style scoped>
+@font-face {
+  font-family: 'Minecraftia';
+  src: url('@/assets/fonts/Minecraftia-Regular.ttf') format('truetype');
+}
+
+#home {
+  font-family: 'Minecraftia', sans-serif;
+  text-align: center;
+  color: #2c3e50;
+  min-height: 100vh;
+  background-size: cover;
+  background-position: center;
+  position: relative;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+
+#home::before {
+  content: "";
+  position: absolute;
+  top: 0;
+  left: 0;
+  right: 0;
+  bottom: 0;
+  background-image: url(../src/assets/1347935.png);
+  background-size: cover;
+  background-position: center;
+  filter: blur(4px);
+  z-index: -1;
+}
+
+.mc-button {
+  height: 50pt;
+  width: calc(30pt * 10); 
+  cursor: pointer;
+  overflow: hidden;
+  white-space: nowrap;
+  user-select: none;
+  background: #999 url('https://i.ibb.co/rb2TWXL/bgbtn.png') center / cover;
+  image-rendering: pixelated;
+  border: 2px solid #000;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  text-align: center;
+}
+
+.mc-button .title {
+  font-size: 1.2em; 
+  margin-top: 15px;
+}
+
+.mc-button:hover {
+  background-color: rgba(100, 100, 255, .45);
+  text-shadow: 2px 2px #202013CC;
+  color: #FFFFA0;
+}
+
+h1 {
+  font-size: 1.8em;
+  font-weight: bold;
+}
+
+nav {
+  display: flex;
+  justify-content: center;
+  gap: 20px;
+  margin-top: 20px;
+}
+
+nav a {
+  color: white;
+  font-size: 1.4em;
+  text-decoration: none;
+  padding: 10px 15px;
+  border-radius: 5px;
+  transition: background-color 0.3s, transform 0.2s;
+  font-weight: bold;
+  transition: background-color 0.3s;
+}
+
+nav a:hover {
+  background-color: #42b983;
+  transform: scale(1.1);
+}
+
+nav a:active {
+  transform: scale(1);
+}
+
+main {
+  margin-top: 40px;
+}
+</style>
