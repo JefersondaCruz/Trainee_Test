28bdaf1840dadd39bae3242152cfa385ca63c755
JefersondaCruz <jefersondacruz40@gmail.com>
2025-02-15 14:58:58 -0300

    DevServices lógica Criada

---
+++ frontend/src/services/DevServices.js
@@ -0,0 +1,51 @@
+import laravelApi from "./HttpService";
+
+
+export const RegisterDev = async (nome, nivel, email, cpf) => {
+    try {
+        const response = await laravelApi.post("/desenvolvedores", {
+            nome,
+            nivel,
+            email,
+            cpf,
+        })
+        return response;
+    } catch (error) {
+        console.error("Erro ao registrar desenvolvedor")
+        throw error;
+    }
+}
+export const GetDevs = async () => {
+    try {
+        const response = await laravelApi.get("/desenvolvedores");
+        return response;
+    } catch (error) {
+        console.error("Erro ao recuperar desenvolvedores")
+        throw error;
+    }
+}
+
+export const UpdateDev = async (id, nome, nivel, email, cpf) => {
+    try {
+        const response = await laravelApi.patch(`/desenvolvedores/${id}`, {
+            nome,
+            nivel,
+            email,
+            cpf,
+        })
+        return response;
+    } catch (error) {
+        console.error("Erro ao atualizar desenvolvedor")
+        throw error;
+    }
+}
+
+export const DeleteDev = async (id) => {
+    try {
+        const response = await laravelApi.delete(`/desenvolvedores/${id}`);
+        return response;
+    } catch (error) {
+        console.error("Erro ao excluir desenvolvedor")
+        throw error;
+    }
+}
